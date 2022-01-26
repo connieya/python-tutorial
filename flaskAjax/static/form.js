869091c1deb8849a2a690160be37d33e804b6817
gunny6026 <gunny6026@naver.com>
2022-01-26 10:52:59 +0900

    feat : 간단한 메모장 구현

--- flaskAjax/static/form.js
+++ flaskAjax/static/form.js
@@ -1,25 +1,30 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-    $('form').on('submit', function(event){
-        alert('ggg')
-        $.ajax({
-            data  : {
-                name : $('#nameInput').val(),
-                email : $('emailInput').val()
-            },
-            type : 'POST',
-            url : '/process'
-        })
-        .done(function(data){
-            if(data.error) {
-                $('#errorAlert').text(data.error).show();
-                $('#successAlert').hide();
-            }else {
-                $('#successAlert').text(data.name).show();
-                $('#errorAlert').hide();
-            }
-        });
-        event.preventDefault();
+	$('form').on('submit', function(event) {
+        alert('제출 완료')
+		$.ajax({
+			data : {
+				name : $('#nameInput').val(),
+				email : $('#emailInput').val()
+			},
+			type : 'POST',
+			url : '/process'
+		})
+		.done(function(data) {
 
-    });
+			if (data.error) {
+				$('#errorAlert').text(data.error).show();
+				$('#successAlert').hide();
+			}
+			else {
+				$('#successAlert').text(data.name).show();
+				$('#errorAlert').hide();
+			}
+
+		});
+
+		event.preventDefault();
+
+	});
+
 });
